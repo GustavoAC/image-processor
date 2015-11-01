94762608697eae11e71ccc75c99ccba0e922e953
GustavoAC <guga.a.carvalho@gmail.com>
2015-11-01 13:13:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "functions.h"
-
+#include "function.h"
 
 int main(){
-	int height, width, i, j, clrRange;
+    FILE *imagepath;
+	int height, width, clrRange;
 	char filetype[3], nome[50], nomeArq[55];
 	header();
-	
+
 	scanf("%s", nome);
 	strcpy(nomeArq, nome);
 	strcat(nomeArq, ".ppm");
 	imagepath = fopen(nomeArq, "r");
-	
+
 	if(imagepath == NULL){
-        system("clear");
-        printf("Esse arquivo não foi encontrado.\nTente novamente:\n");
-        open_image();
+		system("clear");
+		printf("Esse arquivo não foi encontrado.\n");
+		exit(1);
 	}
 
 	fgets(filetype, 3, imagepath);
 	fscanf(imagepath, "%i %i %i", &height, &width, &clrRange);
-	Pixel image[height][width];
-	
-    read_image(height, width, filetype, image);
-	
-	fclose(imagepath);
+    Pixel image[height][width];
+    read_image(height, width, filetype, image, imagepath);
 
-	
-    effect_black_white(nome, height, width, image);
+    ef_thresholding(nome, height, width, &clrRange, image);
     create_new_file(nome, filetype, height, width, clrRange, image);
-	
+
 	return 0;
 }
