fd37df913c073f18baa376fbb4b8f11f45a3c68e
GustavoAC <guga.a.carvalho@gmail.com>
2015-11-04 21:35:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "function.h"
+#include "global.h"
+#include "functions.h"
 
 int main(){
-    FILE *imagepath;
-	int height, width, clrRange;
-	char filetype[3], nome[50], nomeArq[55];
+	FILE *imagepath;
 	header();
 
 	scanf("%s", nome);
@@ -22,11 +21,10 @@ int main(){
 
 	fgets(filetype, 3, imagepath);
 	fscanf(imagepath, "%i %i %i", &height, &width, &clrRange);
-    Pixel image[height][width];
-    read_image(height, width, filetype, image, imagepath);
-
-    ef_thresholding(nome, height, width, &clrRange, image);
-    create_new_file(nome, filetype, height, width, clrRange, image);
+    	Pixel image[height][width];
+    	read_image(image, imagepath);
+	ef_thresholding(image);
+	create_new_file(image);
 
 	return 0;
 }
